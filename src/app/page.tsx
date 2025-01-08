ca20e6e0277bf7a51895cc32e6cad4a59919e20e
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-01-08 17:58:18 +0800

    at school 01-08

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,201 @@
+"use client";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
+type movie = {
+  id: number;
+  name: string;
+};
 
 export default function Home() {
+  const [movies, setMovies] = useState<movie[]>([]);
+  const [name, setName] = useState<string>("");
+  const [rating, setRating] = useState<number>();
+  const [change, setChange] = useState<string>("");
+  const [movie, setMovie] = useState<movie>();
+  const [findMovie, setFindMovie] = useState<number>(0);
+  // fetch all movies
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}`);
+      const data = await res.json();
+      setMovies(data);
+    };
+    fetchData();
+    console.log(movies);
+  }, []);
+  // add new movie
+  async function submit() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/create`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        rating,
+      }),
+    });
+    const respond = await res.json();
+    setMovies(respond);
+    console.log(respond);
+  }
+  // delete
+  async function deletebutton(id: number) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/delete`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id,
+      }),
+    });
+    const respond = await res.json();
+    setMovies(respond);
+    console.log(respond);
+  }
+  // edit
+  async function editSubmit(id: number) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/update`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id,
+        name: change,
+      }),
+    });
+    const respond = await res.json();
+    setMovies(respond);
+    console.log(respond);
+  }
+  // Fine One
+  async function findOneButton(id: number) {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/details/${id}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const respond = await res.json();
+    setMovie(respond);
+    console.log(respond);
+    // const movie1 = movies.find((movie) => movie.id == id);
+    // if (!movie1) {
+    //   return console.log("not found");
+    // } else {
+    //   setMovie(movie1);
+    // }
+    // console.log(movie);
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div>
+      <div className="one">
+        <label>Name</label>
+        <input
+          onChange={(e) => {
+            setName(e.target.value);
+            console.log(name);
+          }}
+          required
+          className="border-border border"
+          type="text"
+          aria-label="Name"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <label>Your Rating</label>
+        <input
+          onChange={(e) => {
+            setRating(Number(e.target.value));
+          }}
+          className="border-border border"
+          type="number"
+          aria-label="ID"
+          required
+        />
+        <button
+          onClick={() => {
+            submit();
+          }}>
+          Submit
+        </button>
+      </div>
+      <div className="two ">
+        <input
+          // value={change}
+          className="border-border border m-7"
+          onChange={(e) => setChange(e.target.value)}
+        />
+        {movies.map((movie) => (
+          <div key={movie.id} className="p-7 flex gap-6">
+            <div>{movie.name}</div>
+            <div>{movie.id}</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            {/* <label htmlFor="name">edit</label> */}
+            <button
+              onClick={() => {
+                deletebutton(movie.id);
+                console.log(movie.id);
+              }}>
+              delete
+            </button>
+            <button
+              onClick={() => {
+                editSubmit(movie.id);
+              }}>
+              Submit
+            </button>
+          </div>
+        ))}
+        <div>
+          <h3>Find Movie</h3>
+          <div>
+            <input
+              type="number"
+              onChange={(e) => {
+                setFindMovie(Number(e.target.value));
+                console.log(findMovie);
+              }}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <button
+              onClick={() => {
+                findOneButton(findMovie);
+                console.log(findMovie);
+              }}>
+              Submit
+            </button>
+            <Suspense>
+              <div key={movie?.id} className="p-7 flex gap-6">
+                <div>{movie?.name ? movie?.name : <p>not found</p>}</div>
+                <div>{movie?.id ? movie?.id : <p>not found</p>}</div>
+
+                {/* <label htmlFor="name">edit</label> */}
+                <button
+                  onClick={() => {
+                    // deletebutton(movie?.id);
+                    // console.log(movie?.id);
+                  }}>
+                  delete
+                </button>
+                <button
+                  onClick={() => {
+                    editSubmit(Number(movie?.id));
+                  }}>
+                  Submit
+                </button>
+              </div>
+            </Suspense>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
